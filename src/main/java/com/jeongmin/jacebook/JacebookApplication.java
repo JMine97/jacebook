4917eb7d1ade9c2f66b1efcb39a96cef9cb03049
JM_CHOI_ <chlwjdals98@gmail.com>
2021-05-12 20:37:14 +0900

    초기 설정

---
+++ src/main/java/com/jeongmin/jacebook/JacebookApplication.java
@@ -0,0 +1,13 @@
+package com.jeongmin.jacebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacebookApplication.class, args);
+	}
+
+}
